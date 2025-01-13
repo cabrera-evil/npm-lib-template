05420708936459e2c4d404f1d21c743e550cc60a
cabrera-evil <douglascabrera.dev@gmail.com>
2025-01-12 22:32:22 -0600

    chore(update): linters and test config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import pluginJs from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -8,4 +9,37 @@ export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: [
+      '**/.husky/**',
+      '**/coverage/**',
+      '**/dist/**',
+      '**/node_modules/**',
+      '**/public/**',
+      '*.d.ts',
+      '*.env*',
+      '.prettierignore',
+      '.prettierrc',
+      'commitlint.config.ts',
+      'eslint.config.*',
+    ],
+  },
+  {
+    rules: {
+      // TypeScript rules
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-require-imports': 'off',
+      // Import/export rules
+      'import/no-unresolved': 'off',
+      'import/extensions': 'off',
+      'import/prefer-default-export': 'off',
+      'import/no-absolute-path': 'off',
+      'import/export': 'off',
+      'global-require': 'off',
+      // Custom JavaScript rules
+      'no-use-before-define': 'warn',
+      'no-shadow': 'off',
+    },
+  },
 ];
